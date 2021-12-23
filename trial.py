65721a7a61f8ba58fba0b6c3a11bb2fa12110dae
nspencerjackson <nspencerjackson@gmail.com>
2021-12-23 08:33:39 +0800

    added trial file to test coordinates

---
+++ trial.py
@@ -0,0 +1,35 @@
+from KeybMouse import *
+
+# x and y coordinates of Temp Tab
+xTemplate = 907
+yTemplate = 85
+# x and y coordinates of Friday Page
+xFriday = 1801
+yFriday = 178
+# x and y coordinates of Weekday Page
+xWeekday = 1799
+yWeekday = 155
+# x and y coordinates of the bottom right Friday Tables
+xBottomFriday = 927
+yBottomFriday = 745
+# x and y coordinates of bottom right Weekday Tables
+xBottomWeekday = 930
+yBottomWeekday = 575
+# x and y coodinates of the top of both tables
+xTopTable = 45
+yTopTable = 236
+# x and y coordinates of new month tab
+xMonth = 263
+yMonth = 90
+# x and y coordinates of Add Page button
+xAddPage = 1776
+yAddPage = 117
+
+moveMouse(xTemplate, yTemplate)
+moveMouse(xFriday, yFriday)
+moveMouse(xWeekday, yWeekday)
+moveMouse(xBottomFriday, yBottomFriday)
+moveMouse(xBottomWeekday, yBottomWeekday)
+moveMouse(xTopTable, yTopTable)
+moveMouse(xMonth, yMonth)
+moveMouse(xAddPage, yAddPage)
