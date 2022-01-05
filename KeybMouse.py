d7e9934ae4cdf4a693636569c01346ec52ee9d8e
nspencerjackson <nspencerjackson@gmail.com>
2022-01-06 06:25:39 +0800

    added comments

--- KeybMouse.py
+++ KeybMouse.py
@@ -4,8 +4,10 @@ import pyautogui as pag
 
 keyboard = Controller()
 
+# Functino to move mouse to x, y coordinates
 def moveMouse(x, y):
     pag.moveTo(x,y,0.3)
 
+# Function to click the left button of the mouse
 def clickMouse():
     pag.click()
